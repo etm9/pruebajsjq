7fd4ddb4ad463b0fd878777af2420baf739287ea
Edgar <etm9@jelpmi.mx>
2016-07-15 21:54:01 -0500

    2 ejercicios resueltos

---
+++ p1/main.js
@@ -0,0 +1,7 @@
+var valor1;
+    var valor2;
+    valor1=prompt('Ingrese primer número:','');
+    valor2=prompt('Ingrese segundo número','');
+    var suma=parseInt(valor1)+parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
